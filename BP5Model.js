22dce32ea76041fb1598492c60e0b083bfbba601
iHealthLabs <jingzhiwei@ihealthlabs.com.cn>
2016-11-04 13:18:20 +0800

    BP5 + UI  + Comment ok

--- BP5Model.js
+++ BP5Model.js
@@ -1,12 +1,29 @@
 'use strict';
 
 
-// import { NativeModules } from 'react-native';
-
-// export default NativeModules.BP5Model;
-
-
-
 var { NativeModules } = require('react-native');
 
-module.exports = NativeModules.BP5Model
+var RCTModule = NativeModules.BP5Model
+
+var BP5Model = {
+
+	//Start measure blood pressure monitor
+	startMeasure: function(
+		//The mac address for blood pressure monitor
+		mac: string
+	): void {
+		RCTModule.startMeasure(mac)
+	},
+
+	//Stop measure blood pressure monitor
+	stopMeasure: function(
+		//The mac address for blood pressure monitor
+		mac: string
+	): void {
+		RCTModule.stopMeasure(mac)
+	}
+
+
+}
+
+module.exports = BP5Model
--- iHealthDeviceManagerModel.js
+++ iHealthDeviceManagerModel.js
@@ -1,11 +1,47 @@
 'use strict';
 
 
-// import { NativeModules } from 'react-native';
-
-// export default NativeModules.iHealthDeviceManagerModel;
-
 
 var { NativeModules } = require('react-native');
 
-module.exports = NativeModules.iHealthDeviceManagerModel
+var RCTModule = NativeModules.iHealthDeviceManagerModel
+
+var iHealthDeviceManagerModel = {
+	//Device Type
+	BP5: RCTModule.BP5,
+	AM4: RCTModule.AM4,
+
+	/**
+ 	 * Start discovery iHealth device with type
+ 	 * Attentation: Please register to receive event('onScanDevice') before call the api. 
+ 	 *
+ 	 * @param {number} type
+ 	 * @return Event('onScanDevice') with a Json object.
+ 	 * eg. {"Type":"BP5","RSSI":0,"Mac":"8CDE5212041A"}
+ 	 * @api public
+ 	*/
+	startDiscovery:function (
+		//iHealth device type, eg. BP5 AM4
+		type: number
+	): void {
+		RCTModule.startDiscovery(type)
+	},
+
+	/**
+ 	 * Connect iHealth device 
+ 	 *
+ 	 * @param {string} mac
+ 	 * @return Event('ConnectionStateChange') with a Json object.
+ 	 * eg. {"Status":1,"ErrorID":0,"Type":"BP5","Mac":"8CDE5212041A"}
+ 	 * @api public
+ 	*/
+	connectDevice:function (
+		//The mac address for blood pressure monitor
+		mac: string
+	): void {
+		return RCTModule.connectDevice(mac)
+	},
+
+}
+
+module.exports = iHealthDeviceManagerModel
