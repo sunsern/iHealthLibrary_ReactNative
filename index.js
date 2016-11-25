c284d7ee43ea4064ec94b28b10b9dbbb06426611
李雪松 <315726048@qq.com>
2016-11-25 18:05:00 +0800

    提交index 增加PO相关

--- index.js
+++ index.js
@@ -11,14 +11,16 @@
 // module.exports = NativeModules;
 
 var Component = {
-    iHealthDeviceManagerModule:require('./Module/iHealthDeviceManagerModule'),
-    BP5Module:require('./Module/BP5Module'),
-    BP3LModule:require('./Module/BP3LModule'),
-    BP550BTModule:require('./Module/BP550BTModule'),
-    BP7SModule:require('./Module/BP7SModule'),
-    BPProfileModule:require('./Module/BPProfileModule'),
-    AM4Module:require('./Module/AM4Module'),
-    AMProfileModule:require('./Module/AMProfileModule')
+    iHealthDeviceManagerModule: require('./Module/iHealthDeviceManagerModule'),
+    BP5Module: require('./Module/BP5Module'),
+    BP3LModule: require('./Module/BP3LModule'),
+    BP550BTModule: require('./Module/BP550BTModule'),
+    BP7SModule: require('./Module/BP7SModule'),
+    BPProfileModule: require('./Module/BPProfileModule'),
+    AM4Module: require('./Module/AM4Module'),
+    AMProfileModule: require('./Module/AMProfileModule'),
+    PO3Module: require('./Module/PO3Module'),
+    POProfileModule: require('./Module/POProfileModule')
 
 }
 
