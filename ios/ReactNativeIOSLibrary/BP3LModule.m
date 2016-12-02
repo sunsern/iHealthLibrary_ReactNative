0cb755bfbf9104a64e2c0eac291b6ece58bc8f44
daiqingquan <476426640@qq.com>
2016-12-02 17:13:55 +0800

    更新静态库支持模拟器真机调试，增加支持所有扫描

--- ios/ReactNativeIOSLibrary/BP3LModule.m
+++ ios/ReactNativeIOSLibrary/BP3LModule.m
@@ -2,22 +2,12 @@
 //  BP3LModule.m
 //  ReactNativeIOSLibrary
 //
-//  Created by Liu Yanbo on 2016/11/23.
-//  Copyright © 2016年 Liu Yanbo. All rights reserved.
+//  Created by daiqingquan on 2016/11/23.
+//  Copyright © 2016年 daiqingquan. All rights reserved.
 //
 
 #import "BP3LModule.h"
 
 @implementation BP3LModule
-
-@synthesize bridge = _bridge;
-
-RCT_EXPORT_MODULE()
-
-#pragma mark
-
-- (NSDictionary *)constantsToExport {
-    return @{@"deviceName":@"BP3L"};
-};
 
 @end
