f8a0c5e0d25107fea9faada5e046e7678ee1d638
Realank <realank@126.com>
2016-12-02 09:58:38 +0800

    change device manager module for BP3L

--- ios/ReactNativeIOSLibrary/iHealthDeviceManagerModule.m
+++ ios/ReactNativeIOSLibrary/iHealthDeviceManagerModule.m
@@ -8,6 +8,7 @@
 
 #import "iHealthDeviceManagerModule.h"
 #import "AMHeader.h"
+#import "BPHeader.h"
 @implementation iHealthDeviceManagerModule
 
 @synthesize bridge = _bridge;
@@ -23,28 +24,46 @@ RCT_EXPORT_MODULE()
     if (self=[super init])
     {
         
-        [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(deviceAM3SDiscover:) name:AM3SDiscover object:nil];
-        [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(deviceConnectForAM3S:) name:AM3SConnectNoti object:nil];
+        [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(deviceDiscover:) name:AM3SDiscover object:nil];
+        [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(deviceConnect:) name:AM3SConnectNoti object:nil];
+        
+        [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(deviceDiscover:) name:BP3LDiscover object:nil];
+        [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(deviceConnect:) name:BP3LConnectNoti object:nil];
         
         [AM3SController shareIHAM3SController];
+        [BP3LController shareBP3LController];
         
         [ScanDeviceController commandGetInstance];
+        
     }
     return self;
 }
 
 #pragma mark
 #pragma mark - Notification
--(void)deviceAM3SDiscover:(NSNotification*)info {
+-(void)deviceDiscover:(NSNotification*)info {
     
-    [self.bridge.eventDispatcher sendAppEventWithName:@"ScanDevice" body:[info userInfo]];
+    NSLog(@"Native: device discover %@", info);
+    //    [self.bridge.eventDispatcher sendAppEventWithName:@"ScanDevice" body:[info userInfo]];
+    NSDictionary* userInfo = [info userInfo];
+    NSDictionary* deviceInfo = @{@"mac":userInfo[@"SerialNumber"],@"type":[self constantsToExport][userInfo[@"DeviceName"]]};
+    [self.bridge.eventDispatcher sendDeviceEventWithName:@"ScanDevice" body:deviceInfo];
     
+}
+-(void)deviceConnect:(NSNotification*)info {
+    //    [self.bridge.eventDispatcher sendAppEventWithName:@"DeviceConnected" body:[info userInfo]];
+    NSDictionary* userInfo = [info userInfo];
+    NSDictionary* deviceInfo = @{@"mac":userInfo[@"SerialNumber"],@"type":[self constantsToExport][userInfo[@"DeviceName"]]};
+    [self.bridge.eventDispatcher sendDeviceEventWithName:@"DeviceConnected" body:deviceInfo];
 }
 #pragma mark
 #pragma mark - constantsToExport
 
 - (NSDictionary *)constantsToExport
+
 {
+    
+    BP3* bp3 = [[BP3LController shareBP3LController] getAllCurrentBP3LInstace].firstObject;
     return @{
              @"AM3" : [NSNumber numberWithInt:HealthDeviceType_AM3],
              @"AM3S" : [NSNumber numberWithInt:HealthDeviceType_AM3S],
@@ -58,12 +77,12 @@ RCT_EXPORT_MODULE()
              @"BP5":@"BP5",
              @"BG1":@"BG1",
              @"BG5":@"BG5",
-             @"HS6":@"BG5",
-             @"ScanDevice":@"ScanDevice",
-             @"ScanFinish":@"ScanFinish",
-              @"DeviceConnected":@"DeviceConnected",
-              @"DeviceConnectFailed":@"DeviceConnectFailed",
-              @"DeviceDisconnect":@"DeviceDisconnect"
+             @"HS6":@"HS6",
+             @"Event_Scan_Device":@"ScanDevice",
+             @"Event_Scan_Finish":@"ScanFinish",
+             @"Event_Device_Connected":@"DeviceConnected",
+             @"Event_Device_Connect_Failed":@"DeviceConnectFailed",
+             @"Event_Device_Disconnect":@"DeviceDisconnect"
              
              };
 };
@@ -76,7 +95,7 @@ RCT_EXPORT_METHOD(startDiscovery:(nonnull NSNumber *)deviceType){
     
     [[ScanDeviceController commandGetInstance] commandScanDeviceType:[deviceType intValue]];
     
-
+    
     
 }
 
@@ -87,7 +106,8 @@ RCT_EXPORT_METHOD(stopDiscovery:(nonnull NSNumber *)deviceType){
     [[ScanDeviceController commandGetInstance] commandStopScanDeviceType:[deviceType intValue]];
     
     
-     [self.bridge.eventDispatcher sendAppEventWithName:@"ScanFinish" body:nil];
+    //     [self.bridge.eventDispatcher sendAppEventWithName:@"ScanFinish" body:nil];
+    [self.bridge.eventDispatcher sendDeviceEventWithName:@"ScanFinish" body:nil];
 }
 
 RCT_EXPORT_METHOD(connectDevice:(nonnull NSString *)mac type:(nonnull NSNumber *)deviceType){
