dd4ef7547a54590e3d4a306914c0168ff5fdc773
Realank <realank@126.com>
2016-12-05 11:24:10 +0800

    [iOS]update BP3L

--- ios/ReactNativeIOSLibrary/BP3LModule.h
+++ ios/ReactNativeIOSLibrary/BP3LModule.h
@@ -2,12 +2,16 @@
 //  BP3LModule.h
 //  ReactNativeIOSLibrary
 //
-//  Created by daiqingquan on 2016/11/23.
-//  Copyright © 2016年 daiqingquan. All rights reserved.
+//  Created by Liu Yanbo on 2016/12/05.
+//  Copyright © 2016年 Liu Yanbo. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
-@interface BP3LModule : NSObject
+#import "RCTBridgeModule.h"
+#import "RCTBridge.h"
+#import "RCTEventDispatcher.h"
+#import "ScanDeviceController.h"
+#import "ConnectDeviceController.h"
+@interface BP3LModule : NSObject<RCTBridgeModule>
 
 @end
