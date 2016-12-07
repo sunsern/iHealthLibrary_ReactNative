01bb72bb4e3f551ecefcf14468fcb8a3d975d641
YONGGUANG ZHAO <zhygking@163.com>
2016-12-07 10:32:36 +0800

    BG1View

--- Example/BG1View.js
+++ Example/BG1View.js
@@ -13,7 +13,8 @@ import {
 
 import {
     iHealthDeviceManagerModule,
-    BG1Module
+    BG1Module,
+    BG1ProfileModule
 } from 'ihealthlibrary-react-native'
 
 
@@ -23,7 +24,8 @@ var styles = StyleSheet.create({
         marginTop: 50
     },
     contentContainer: {
-        height: 400,
+        height: 200,
+        marginTop: 20
     },
     // 按钮
     button: {
@@ -70,16 +72,19 @@ export default class BG1View extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            QRCode:"02ABCDE67C284BA29ACDFEE6E60A2FE43EDF0C"
+        }
 
     }
 
     componentWillMount() {
-        console.info('BG1View', 'componentWillMount', null);
-
+        console.info('BG1View', 'componentWillMount', '_addListener');
+         this._addListener();
     }
 
     componentDidMount() {
-        console.info('BG1View', 'componentDidMount', null);
+        console.info('BG1View', 'componentDidMount',  null);
     }
 
 
@@ -100,8 +105,72 @@ export default class BG1View extends Component {
     }
 
     componentWillUnmount() {
-        console.info('BG1View', 'componentWillUnmount', null);
+        console.info('BG1View', 'componentWillUnmount', '_removeListener');
+         this._removeListener();
     }
+
+
+    _addListener() {
+        let self = this;
+
+        this.connectionListener = DeviceEventEmitter.addListener(iHealthDeviceManagerModule.Event_Device_Disconnect, function (e: Event) {
+            // handle event.
+            console.info('BG1View', 'addListener_DeviceDisconnect', JSON.stringify(e));
+            self.props.navigator.pop();
+        });
+
+        this.notifyListener = DeviceEventEmitter.addListener(BG1Module.Event_Notify, function (e: Event) {
+
+            console.info('BG1View', 'Event_Notify',"Action = " +  e.action + '\n' + "Message = " +  JSON.stringify(e));
+            
+            if(e.action===BG1ProfileModule.ACTION_BG1_SENDCODE_RESULT) {
+                console.info(JSON.stringify(e));
+            } else if (e.action === BG1ProfileModule.ACTION_BG1_MEASURE_ERROR) {
+                console.info(JSON.stringify(e));
+            } else if (e.action === BG1ProfileModule.ACTION_BG1_MEASURE_STRIP_IN) {
+                 console.info(JSON.stringify(e));
+            } else if (e.action === BG1ProfileModule.ACTION_BG1_MEASURE_GET_BLOOD) {
+                 console.info(JSON.stringify(e));
+            } else if (e.action === BG1ProfileModule.ACTION_BG1_MEASURE_RESULT) {
+                 console.info(JSON.stringify(e));
+            } else if (e.action === BG1ProfileModule.ACTION_BG1_MEASURE_STRIP_OUT) {
+                 console.info(JSON.stringify(e));
+            } else if (e.action === BG1ProfileModule.ACTION_BG1_MEASURE_STANDBY) {
+                 console.info(JSON.stringify(e));
+            }
+            self.refs.TipView.setState({tip: JSON.stringify(e)});
+
+        });
+    }
+
+    _removeListener() {
+        //Unregister  event
+        if (this.connectionListener) {
+            this.connectionListener.remove()
+        }
+        if (this.notifyListener) {
+            this.notifyListener.remove()
+        }
+    }
+
+    // _connect() {
+    //     console.info('----------------------------connect bg1 device------------------------------')
+    //     BG1Module.connect();
+    // }
+
+    _sendCode() {
+        BG1Module.sendCode(this.state.QRCode);
+    }
+
+    _getBottleInfoFromQR() {
+       BG1Module.getBottleInfoFromQR(this.state.QRCode);
+    }
+
+    // _disconnect() {
+    //     console.info('----------------------------disconnect bg1 device------------------------------')
+    //     BG1Module.disconnect();
+    // }
+
 
     render() {
 
@@ -112,35 +181,21 @@ export default class BG1View extends Component {
 
                     <TouchableOpacity
                         style={styles.button}
-                        onPress={() => this._getDeviceIDPS()}>
+                        onPress={() => this._getBottleInfoFromQR()}>
                         <Text style={styles.buttonText}>
-                            连接设备
+                            解析Code信息
                         </Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity
                         style={styles.button}
-                        onPress={() => this._startMeasure()}>
+                        onPress={() => this._sendCode()}>
                         <Text style={styles.buttonText}>
                             发送Code
                         </Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => this._stopMeasure()}>
-                        <Text style={styles.buttonText}>
-                            断开设备
-                        </Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={styles.button}
-                        onPress={() => this._getCodeInfoFromQR()}>
-                        <Text style={styles.buttonText}>
-                            解析Code信息
-                        </Text>
-                    </TouchableOpacity>
+                    
                 </ScrollView>
 
 
@@ -149,8 +204,4 @@ export default class BG1View extends Component {
         )
     }
 
-
-    _getCodeInfoFromQR() {
-       
-    }
 }
