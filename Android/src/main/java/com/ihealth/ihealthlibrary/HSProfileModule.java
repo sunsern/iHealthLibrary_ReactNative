fdef0d877e050aaef4ce22078bf7fd4b0f001dea
李雪松 <315726048@qq.com>
2016-12-07 10:04:30 +0800

    1.增加HS4S注释 2.修改PO3的一些问题

--- Android/src/main/java/com/ihealth/ihealthlibrary/HSProfileModule.java
+++ Android/src/main/java/com/ihealth/ihealthlibrary/HSProfileModule.java
@@ -17,23 +17,26 @@ public class HSProfileModule extends ReactContextBaseJavaModule {
     private static final String modelName = "HSProfileModule";
     private static final String TAG = "HSProfileModule";
 
-    private static final String ACTION_ERROR_HS = "ACTION_ERROR_HS";
-    private static final String ACTION_ONLINE_RESULT_HS = "ACTION_ONLINE_RESULT_HS";
-    private static final String ACTION_HISTORICAL_DATA_HS = "ACTION_HISTORICAL_DATA_HS";
-    private static final String ACTION_HISTORICAL_NUM_HS = "ACTION_HISTORICAL_NUM_HS";
-    private static final String ACTION_NO_HISTORICALDATA = "ACTION_NO_HISTORICALDATA";
-    private static final String ACTION_BATTERY_HS = "ACTION_BATTERY_HS";
     private static final String ACTION_LIVEDATA_HS = "ACTION_LIVEDATA_HS";
-    private static final String ACTION_STABLEDATA_HS = "ACTION_STABLEDATA_HS";
-    private static final String ACTION_IMPEDANCEDATA_HS = "ACTION_IMPEDANCEDATA_HS";
-    private static final String ACTION_MANAGEMENT_HS = "ACTION_MANAGEMENT_HS";
-    private static final String ACTION_ADDUSER_HS = "ACTION_ADDUSER_HS";
-    private static final String ACTION_DELETEUSER_HS = "ACTION_DELETEUSER_HS";
-    private static final String ACTION_UPDATEUSER_HS = "ACTION_UPDATEUSER_HS";
-    private static final String ACTION_SETTINGWIFI = "ACTION_SETTINGWIFI";
-    private static final String ACTION_SETWIFI_SUCCESS = "ACTION_SETWIFI_SUCCESS";
-    private static final String ACTION_SETWIFI_FAIL = "ACTION_SETWIFI_FAIL";
-    private static final String ACTION_SETWIFI_UNKNOW = "ACTION_SETWIFI_UNKNOW";
+    private static final String LIVEDATA_HS = "LIVEDATA_HS";
+
+    private static final String ACTION_ONLINE_RESULT_HS = "ACTION_ONLINE_RESULT_HS";
+    private static final String DATAID = "DATAID";
+    private static final String WEIGHT_HS = "WEIGHT_HS";
+    private static final String FAT_HS = "FAT_HS";
+    private static final String WATER_HS = "WATER_HS";
+    private static final String MUSCLE_HS = "MUSCLE_HS";
+    private static final String SKELETON_HS = "SKELETON_HS";
+    private static final String FATELEVEL_HS = "FATELEVEL_HS";
+    private static final String DCI_HS = "DCI_HS";
+
+    private static final String ACTION_HISTORICAL_DATA_HS = "ACTION_HISTORICAL_DATA_HS";
+    private static final String HISTORDATA__HS = "HISTORDATA__HS";
+    private static final String MEASUREMENT_DATE_HS = "MEASUREMENT_DATE_HS";
+
+    private static final String ACTION_NO_HISTORICALDATA = "ACTION_NO_HISTORICALDATA";
+
+    private static final String ACTION_ERROR_HS = "ACTION_ERROR_HS";
 
     public HSProfileModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -48,23 +51,26 @@ public class HSProfileModule extends ReactContextBaseJavaModule {
     @Override
     public Map<String, Object> getConstants() {
         Map<String, Object> constants = new HashMap<>();
-        constants.put(ACTION_ERROR_HS, HsProfile.ACTION_ERROR_HS);
-        constants.put(ACTION_ONLINE_RESULT_HS, HsProfile.ACTION_ONLINE_RESULT_HS);
-        constants.put(ACTION_HISTORICAL_DATA_HS, HsProfile.ACTION_HISTORICAL_DATA_HS);
-        constants.put(ACTION_HISTORICAL_NUM_HS, HsProfile.ACTION_HISTORICAL_NUM_HS);
-        constants.put(ACTION_NO_HISTORICALDATA, HsProfile.ACTION_NO_HISTORICALDATA);
-        constants.put(ACTION_BATTERY_HS, HsProfile.ACTION_BATTERY_HS);
         constants.put(ACTION_LIVEDATA_HS, HsProfile.ACTION_LIVEDATA_HS);
-        constants.put(ACTION_STABLEDATA_HS, HsProfile.ACTION_STABLEDATA_HS);
-        constants.put(ACTION_IMPEDANCEDATA_HS, HsProfile.ACTION_IMPEDANCEDATA_HS);
-        constants.put(ACTION_MANAGEMENT_HS, HsProfile.ACTION_MANAGEMENT_HS);
-        constants.put(ACTION_ADDUSER_HS, HsProfile.ACTION_ADDUSER_HS);
-        constants.put(ACTION_DELETEUSER_HS, HsProfile.ACTION_DELETEUSER_HS);
-        constants.put(ACTION_UPDATEUSER_HS, HsProfile.ACTION_UPDATEUSER_HS);
-        constants.put(ACTION_SETTINGWIFI, HsProfile.ACTION_SETTINGWIFI);
-        constants.put(ACTION_SETWIFI_SUCCESS, HsProfile.ACTION_SETWIFI_SUCCESS);
-        constants.put(ACTION_SETWIFI_FAIL, HsProfile.ACTION_SETWIFI_FAIL);
-        constants.put(ACTION_SETWIFI_UNKNOW, HsProfile.ACTION_SETWIFI_UNKNOW);
+        constants.put(LIVEDATA_HS, HsProfile.LIVEDATA_HS);
+
+        constants.put(ACTION_ONLINE_RESULT_HS, HsProfile.ACTION_ONLINE_RESULT_HS);
+        constants.put(HISTORDATA__HS, HsProfile.HISTORDATA__HS);
+        constants.put(DATAID, HsProfile.DATAID);
+        constants.put(WEIGHT_HS, HsProfile.WEIGHT_HS);
+        constants.put(FAT_HS, HsProfile.FAT_HS);
+        constants.put(WATER_HS, HsProfile.WATER_HS);
+        constants.put(MUSCLE_HS, HsProfile.MUSCLE_HS);
+        constants.put(SKELETON_HS, HsProfile.SKELETON_HS);
+        constants.put(FATELEVEL_HS, HsProfile.FATELEVEL_HS);
+        constants.put(DCI_HS, HsProfile.DCI_HS);
+
+        constants.put(ACTION_HISTORICAL_DATA_HS, HsProfile.ACTION_HISTORICAL_DATA_HS);
+        constants.put(MEASUREMENT_DATE_HS, HsProfile.MEASUREMENT_DATE_HS);
+
+        constants.put(ACTION_NO_HISTORICALDATA, HsProfile.ACTION_NO_HISTORICALDATA);
+
+        constants.put(ACTION_ERROR_HS, HsProfile.ACTION_ERROR_HS);
 
         return constants;
     }
