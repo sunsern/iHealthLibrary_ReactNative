c9dbd3b19bc64d8cc2c1bc2d16c97a8ffcf50a6e
Realank <realank@126.com>
2016-12-12 11:37:07 +0800

    add function to bp7s

--- ios/ReactNativeIOSLibrary/BP7SModule.m
+++ ios/ReactNativeIOSLibrary/BP7SModule.m
@@ -58,6 +58,102 @@ RCT_EXPORT_METHOD(getFunctionInfo:(nonnull NSString *)mac){
     
 }
 
+//getOffLineNum
+RCT_EXPORT_METHOD(getOffLineNum:(nonnull NSString *)mac){
+    
+    if ([self getDeviceWithMac:mac]!=nil) {
+        [[self getDeviceWithMac:mac] commandTransferMemorytotalCount:^(NSNumber *num) {
+            [self sendEventWithAction:@"ACTION_HISTORICAL_NUM_BP" keyString:@"count" valueString:num];
+        } errorBlock:^(BPDeviceError error) {
+            [self sendErrorWithCode:error];
+        }];
+    }else{
+        NSLog(@"error %d",BPDidDisconnect);
+        [self sendErrorWithCode:BPDidDisconnect];
+    }
+    
+    
+}
+
+//getOffLineData
+RCT_EXPORT_METHOD(getOffLineData:(nonnull NSString *)mac){
+    
+    if ([self getDeviceWithMac:mac]!=nil) {
+        [[self getDeviceWithMac:mac] commandTransferMemoryDataWithUser:@"heds@12.com" clientID:@"2a8387e3f4e94407a3a767a72dfd52ea" clientSecret:@"fd5e845c47944a818bc511fb7edb0a77" Authentication:^(UserAuthenResult result) {
+            NSLog(@"authen %d",result);
+            if (result != UserAuthen_LoginSuccess) {
+                [self sendErrorWithCode:result];
+            }
+        } totalCount:^(NSNumber *num) {
+            [self sendEventWithAction:@"ACTION_HISTORICAL_NUM_BP" keyString:@"value" valueString:num];
+        } pregress:^(NSNumber *pregress) {
+            NSLog(@"pressure %@",pregress);
+            [self sendEventWithAction:@"ACTION_ONLINE_PRESSURE_BP" keyString:@"value" valueString:pregress];
+        } dataArray:^(NSArray *array) {
+            NSLog(@"dataArray %@",array);
+            [self sendEventWithAction:@"ACTION_HISTORICAL_DATA_BP" keyString:@"value" valueString:array];
+        } errorBlock:^(BPDeviceError error) {
+            NSLog(@"error %d",error);
+            [self sendErrorWithCode:error];
+        }];
+    }else{
+        NSLog(@"error %d",BPDidDisconnect);
+        [self sendErrorWithCode:BPDidDisconnect];
+    }
+    
+    
+}
+//getBattery
+RCT_EXPORT_METHOD(getBattery:(nonnull NSString *)mac){
+    
+    if ([self getDeviceWithMac:mac]!=nil) {
+        [[self getDeviceWithMac:mac] commandEnergy:^(NSNumber *energyValue) {
+            NSLog(@"energyValue %@",energyValue);
+            [self sendEventWithAction:@"ACTION_BATTERY_BP" keyString:@"battery" valueString:energyValue];
+        } errorBlock:^(BPDeviceError error) {
+            NSLog(@"error %d",error);
+            [self sendErrorWithCode:error];
+        }];
+    }else{
+        NSLog(@"error %d",BPDidDisconnect);
+        [self sendErrorWithCode:BPDidDisconnect];
+    }
+    
+    
+}
+//setUnit
+RCT_EXPORT_METHOD(setUnit:(nonnull NSString *)mac unit:(NSString*)unit){
+    
+    if ([self getDeviceWithMac:mac]!=nil) {
+        [[self getDeviceWithMac:mac] commandSetUnit:unit errorBlock:^(BPDeviceError error) {
+            NSLog(@"error %d",error);
+            [self sendErrorWithCode:error];
+        }];
+    }else{
+        NSLog(@"error %d",BPDidDisconnect);
+        [self sendErrorWithCode:BPDidDisconnect];
+    }
+    
+    
+}
+//angleSet
+RCT_EXPORT_METHOD(angleSet:(nonnull NSString *)mac hl:(NSNumber*)hl ll:(NSNumber*)ll hr:(NSNumber*)hr lr:(NSNumber*)lr){
+    
+    if ([self getDeviceWithMac:mac]!=nil) {
+        NSDictionary* dict = @{@"highAngleForLeft":hl, @"lowAngleForLeft":ll, @"highAngleForRight":hr, @"lowAngleForRight":lr};
+        [[self getDeviceWithMac:mac] commandSetAngle:dict errorBlock:^(BPDeviceError error) {
+            NSLog(@"error %d",error);
+            [self sendErrorWithCode:error];
+        }];
+    }else{
+        NSLog(@"error %d",BPDidDisconnect);
+        [self sendErrorWithCode:BPDidDisconnect];
+    }
+    
+    
+}
+
+
 - (void)sendErrorWithCode:(NSInteger)errorCode{
     [self sendEventWithAction:@"ACTION_ERROR_BP" keyString:@"value" valueString:@(errorCode)];
 }
@@ -65,5 +161,8 @@ RCT_EXPORT_METHOD(getFunctionInfo:(nonnull NSString *)mac){
 - (void)sendEventWithAction:(NSString*)actionName keyString:(NSString*)key valueString:(id)value{
     [self.bridge.eventDispatcher sendDeviceEventWithName:@"BP7S.MODULE.NOTIFY"  body:@{@"action":actionName,key:value}];
 }
+
+
+
 
 @end
